725f59c98a58d51867fbb138b9e00b5ce95ebfe7
ShubhPundir <shb.pndr@gmail.com>
2025-01-21 22:19:10 +0530

    Problem 11

---
+++ Container with Most Water/Soln.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    // vector<int> height = {1,8,6,2,5,4,8,3,7};
+    vector<int> height = {4,1,3,1};
+    int left = 0;
+    int right = height.size() - 1;
+
+    int maxWater = 0;
+
+    while (left < right)
+    {
+        int distance = right - left;
+        int currWater = distance * min(height[right],height[left]);
+        maxWater = max(maxWater,currWater);
+
+        if(height[right] < height[left]){
+            right--;
+        } else {
+            left ++;
+        }
+    }
+
+    cout << maxWater;
+    
+    
+}
