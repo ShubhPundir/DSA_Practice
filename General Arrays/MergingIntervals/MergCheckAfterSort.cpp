b51290f263d73fe07f5c2cd03a29c83f88bb9809
ShubhPundir <shb.pndr@gmail.com>
2025-06-17 16:21:07 +0530

    Merge conflicts between two sorted arrays of pairs

---
+++ General Arrays/MergingIntervals/MergCheckAfterSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size(); i++){
+            if(ans.empty() || nums[i][0] > ans.back()[1]){
+                // no overlap or cold start
+                ans.push_back(nums[i]);
+            } else {
+                // overlap
+                ans.back()[1] = max(ans.back()[1], nums[i][1]);
+            }
+        }    
+        return ans;
+    }
+};
+
+int main(){
+    Solution solm;
+    vector<vector<int>> nums = {{1, 3}, {2, 6}, {5, 8}, {2, 10}, {1, 7}, {4, 9}};
+    
+    vector<vector<int>> ans = solm.merge(nums);
+    for(auto a: ans){
+        for(auto i: a){
+            cout << i << "-";
+        }
+        cout << ",";
+    }
+    
+
+
+}
