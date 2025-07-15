1f644d830c6cd05b7b196be5046357d69b1b233e
ShubhPundir <shb.pndr@gmail.com>
2025-07-15 17:16:58 +0530

    update: added post sort comment for a clearer understanding

--- General Arrays/MergingIntervals/MergCheckAfterSort.cpp
+++ General Arrays/MergingIntervals/MergCheckAfterSort.cpp
@@ -10,7 +10,7 @@ public:
         vector<vector<int>> ans;
 
         sort(nums.begin(), nums.end());
-
+        // {{1,3}, {1,7}, {2,6}, {2,10}, {4,9}, {5,8}}
         for(int i = 0; i < nums.size(); i++){
             if(ans.empty() || nums[i][0] > ans.back()[1]){
                 // no overlap or cold start
