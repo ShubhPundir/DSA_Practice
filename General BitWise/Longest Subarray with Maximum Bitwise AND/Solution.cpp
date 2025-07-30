586e504389dbfc10439de363f67f4a354aa0d007
ShubhPundir <shb.pndr@gmail.com>
2025-07-30 14:36:39 +0530

    Maximum BITWISE AND for longest subarray

---
+++ General BitWise/Longest Subarray with Maximum Bitwise AND/Solution.cpp
@@ -0,0 +1,15 @@
+
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxi = *max_element(nums.begin(), nums.end());
+        int cons = 1, res = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] && nums[i] == maxi) cons++;
+            else cons = 1;
+            res = max(res, cons);
+        }
+        return res;
+    }
+};
