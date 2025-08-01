645951a51f03ff0c6e970719cb1c4c3de0f94023
ShubhPundir <shb.pndr@gmail.com>
2025-08-01 23:03:49 +0530

    Nth root with large number optimization

--- General Binary Search/Nth Root/NthRoot.cpp
+++ General Binary Search/Nth Root/NthRoot.cpp
@@ -15,7 +15,21 @@ public:
             }
         }
         return int(ans);
+    } // but we don't really need the actual answer, only if if == m, >= m or <= m
+
+    int func(int mid, int n, int target){
+        long long ans = 1;
+
+        for(int i = 1; i <= n; i++){
+            ans *= mid;
+            if(ans > target) return 2;
+        }
+        if(ans == target) return 1;
+        return 0;
     }
+    // return 1 if == target
+    // return 0 if < target
+    // return 2 if > target
     int nthRoot(int target, int n){
         int ans = -1;
 
@@ -23,10 +37,13 @@ public:
 
         while(low <= high){
             int mid = (low + high) / 2;
-            int nth = poww(mid, n);
+            // int nth = poww(mid, n);
+            int midN = func(mid, n, target);
 
-            if(nth <= target){
+            if(midN == 1){
                 ans = mid;
+                high = mid - 1;
+            } else if(midN == 0){
                 low = mid + 1;
             } else {
                 high = mid - 1;
@@ -39,5 +56,5 @@ public:
 
 int main(){
     Solution soln;
-    cout << soln.nthRoot(16, 2);
+    cout << soln.nthRoot(17, 2);
 }
