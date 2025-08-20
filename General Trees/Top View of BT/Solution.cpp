026e47b1c6c3609b83e93d36f6fc786dacfe7fb3
ShubhPundir <shb.pndr@gmail.com>
2025-08-20 21:14:04 +0530

    Top View Traversal

---
+++ General Trees/Top View of BT/Solution.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int val;
+    Node* left;
+    Node* right;
+    Node(int x): val(x), left(nullptr), right(nullptr) {};
+};
+
+class Solution{
+public:
+    vector<int> topView(Node* root){
+        // top view are for those with a single parent
+        // imagine it with coordinates of x = 0 as root, and + and - both sides
+        vector<int> results;
+        bfs(root, results);
+        return results;
+    }
+    void bfs(Node* root, vector<int>& results){
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
+        map<int, int> mpp; // pos, val
+        mpp[0] = root->val;
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+
+            Node* node = it.first; int pos = it.second;
+
+            if(mpp.find(pos) == mpp.end()){
+                mpp[pos] = node->val;
+            }
+
+            if(node->left != nullptr){
+                q.push({node->left, pos - 1});
+            }
+            if(node->right != nullptr){
+                q.push({node->right, pos + 1});
+            }
+        }
+        for(auto it : mpp){
+            results.push_back(it.second);
+        }
+    }
+
+};
+
+int main() {
+    // Creating a sample binary tree
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(4);
+    root->left->right = new Node(10);
+    root->left->left->right = new Node(5);
+    root->left->left->right->right = new Node(6);
+    root->right = new Node(3);
+    root->right->right = new Node(10);
+    root->right->left = new Node(9);
+
+    Solution solution;
+
+    // Get the top view traversal
+    vector<int> topView =
+                    solution.topView(root);
+
+    // Print the result
+    cout << "Top View Traversal: "<< endl;
+    for(auto node: topView){
+        cout << node << " ";
+    }
+
+    return 0;
+}
