f304a29f92e4e336fff067652880deb0655d1466
ShubhPundir <shb.pndr@gmail.com>
2025-09-02 14:43:39 +0530

    Nodes at distance of K

---
+++ General Trees/Nodes at a distance of K/Solution.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> ans;
+        unordered_map<int, TreeNode*> parent;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode* temp = q.front(); q.pop();
+
+                if(temp->left){
+                    q.push(temp->left);
+                    parent[temp->left->val] = temp;
+                }
+                if(temp->right){
+                    q.push(temp->right);
+                    parent[temp->right->val] = temp;
+                }
+            }
+        }
+
+        unordered_map<int, int> visited;
+        q.push(target);
+        while(k-- && !q.empty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                auto top = q.front(); q.pop();
+
+                visited[top->val] = 1;
+                
+                if(top->left && !visited[top->left->val]){
+                    q.push(top->left);
+                }
+                
+                if(top->right && !visited[top->right->val]){
+                    q.push(top->right);
+                }
+
+                if(parent[top->val] && !visited[parent[top->val] -> val]){
+                    q.push(parent[top->val]);
+                }
+            }
+        }
+
+        while(!q.empty()){
+            ans.push_back(q.front()->val);
+            q.pop();
+        }
+
+
+        return ans;
+    }
+};
